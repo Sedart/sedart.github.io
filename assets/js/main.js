178239c9c15533bcfdcd8e39d11390d523a80f35
Sedart <davidfmpixel@gmail.com>
2018-05-01 19:08:06 +0200

    Fixing layout distribution on different devices

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,7 +17,7 @@
 	};
 
 	skel.breakpoints({
-		xlarge: '(max-width: 1800px)',
+		xlarge: '(max-width: 1800px)',//1800
 		large: '(max-width: 1280px)',
 		medium: '(max-width: 980px)',
 		small: '(max-width: 736px)',
